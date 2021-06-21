3a40eeeb224accb7418c10400c87651a62a306f2
sebafudi <sebastianfudalej@gmail.com>
2021-06-21 21:27:46 +0000

    initial commit

---
+++ customrcInstall.sh
@@ -0,0 +1,21 @@
+if (grep -F 'source ~/.customrc' ~/.bashrc &> /dev/null) then
+  echo ".customrc already found in .bashrc"
+else
+  echo "Adding source ~/.customrc to .bashrc file"
+  printf "\n" >> ~/.bashrc
+  printf "source ~/.customrc" >> ~/.bashrc
+  printf "\n" >> ~/.bashrc
+fi
+
+if ! type "exa" &> /dev/null; then
+  echo "No exa command detected. Installing..."
+  temp=$(curl -s https://api.github.com/repos/ogham/exa/releases/latest \
+    | grep "browser_download_url" \
+    | grep "exa-linux-x86_64-v" \
+    | grep "zip" \
+    | cut -d '"' -f 4)
+  wget $temp -q --show-progress
+  temp=$(echo $temp | cut -d "/" -f 9)
+  unzip $temp bin/exa
+  rm $temp
+fi
